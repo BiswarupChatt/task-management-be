c30b4dd7ac60912497396eb860f286feccfe7903
supreethbs94 <supreethbs94@gmail.com>
2024-04-29 20:22:02 +0100

    task model code

---
+++ app/models/task-model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const {Schema, model} = mongoose
+
+const taskSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    priority: { type: String, enum: ['Low', 'Medium', 'High'], required: true },
+    status: { type: String, enum: ['Pending', 'In Progress', 'Completed'], default: 'Pending' },
+    dueDate: { type: Date, required: true },
+    
+},{timestamps:true})
+
+const Task = model('Task', taskSchema)
+
+module.exports = Task 
