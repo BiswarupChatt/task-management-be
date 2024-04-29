3fe74bcd8ffbccf5e69f936354b521cc2750480f
supreethbs94 <supreethbs94@gmail.com>
2024-04-29 19:57:46 +0100

    Comment model code

--- app/models/comment-model.js
+++ app/models/comment-model.js
@@ -1,1 +1,12 @@
-nknknknkn
+const mongoose = require('mongoose')
+
+const commentSchema = new Schema({
+    content: { type: String, required: true },
+    task: { type: Schema.Types.ObjectId, ref: 'Task', required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    postedAt: { type: Date, default: Date.now }
+},{timestamps:true}) // timestamps true to know when was the comment posted 
+
+const Comment = model('Comment', commentSchema)
+
+module.exports = Comment
