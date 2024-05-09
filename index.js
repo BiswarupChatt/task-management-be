6dc6db20991041284bd20f47964ad06583ba4cb6
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-05-09 13:30:56 +0530

    code beautified

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
 require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const { checkSchema, validationResult } = require('express-validator')
-
+const { checkSchema } = require('express-validator')
 
 const configureDB = require('./config/db')
+
 const userCtrl = require('./app/controllers/user-ctrl')
 const taskCtrl = require('./app/controllers/task-ctrl')
+const commentCtrl = require('./app/controllers/comment-ctrl')
+const timeCtrl = require('./app/controllers/timeLog-ctrl')
 
 const { userRegisterValidation, userLoginValidations, userUpdateValidations } = require('./app/validations/user-validation')
 const { taskValidations, taskStatusValidation } = require('./app/validations/task-validations')
-
 const { commentValidation, commentEditValidation } = require('./app/validations/comment-validation')
-
-const  {timeLogValidations,timeLogUpdateValidations}  = require('./app/validations/timeLog-validations')
- 
+const { timeLogValidations, timeLogUpdateValidations } = require('./app/validations/timeLog-validations')
 
 const authenticateUser = require('./app/middlewares/authenticateUser')
 const authorizeUser = require('./app/middlewares/authorizeUser')
-
-const commentCtrl = require('./app/controllers/comment-ctrl')
-
-const timeCtrl = require('./app/controllers/timeLog-ctrl')
-
-
-
-
-// const emailCtrl = require('./app/controllers/email.ctrl')
-// const { emailValidations } = require('./app/validations/email-validations')
 
 const app = express()
 const port = process.env.PORT
 
 configureDB()
+
 app.use(express.json())
 app.use(morgan('common'))
 app.use(cors())
@@ -52,7 +43,6 @@ app.put('/tasks/statusUpdate', authenticateUser, authorizeUser(["Employee"]), ch
 app.put('/tasks/update', authenticateUser, authorizeUser(["TeamLead"]), checkSchema(taskValidations), taskCtrl.update)
 app.delete('/tasks/delete', authenticateUser, authorizeUser(["TeamLead"]), taskCtrl.delete)
 
-
 //comment crud operation
 app.post('/comment/create', authenticateUser, checkSchema(commentValidation), commentCtrl.create)
 app.get('/comment/get', authenticateUser, commentCtrl.get)
@@ -60,11 +50,10 @@ app.put("/comment/edit", authenticateUser, checkSchema(commentEditValidation), c
 app.delete("/comment/delete", authenticateUser, commentCtrl.delete)
 
 //timeLog crud operations
-app.post('/timeLog/create',authenticateUser,checkSchema(timeLogValidations),timeCtrl.addTimeLog)
-app.get('/timeLog/tasks/:taskId',authenticateUser,timeCtrl.getTimeLogsByTask)
-app.put('/timelog/update',authenticateUser,checkSchema(timeLogUpdateValidations),timeCtrl.update)
-app.delete('/timeLog/delete',authenticateUser,timeCtrl.delete)
-
+app.post('/timeLog/create', authenticateUser, checkSchema(timeLogValidations), timeCtrl.addTimeLog)
+app.get('/timeLog/tasks/:taskId', authenticateUser, timeCtrl.getTimeLogsByTask)
+app.put('/timelog/update', authenticateUser, checkSchema(timeLogUpdateValidations), timeCtrl.update)
+app.delete('/timeLog/delete', authenticateUser, timeCtrl.delete)
 
 app.listen(port, () => {
     console.log(`server is running successfully on this url http://localhost:${port}`)
